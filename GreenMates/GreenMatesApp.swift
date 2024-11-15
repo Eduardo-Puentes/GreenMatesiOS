a4f46d8bc05cf31fa91353a7f147f08fd0760dcb
Eduardo-Puentes <eduardo.puentes.dev@gmail.com>
2024-11-15 07:34:26 -0600

    Initial Commit

---
+++ GreenMates/GreenMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenMatesApp.swift
+//  GreenMates
+//
+//  Created by base on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GreenMatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreenMatesTests/GreenMatesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreenMatesTests.swift
+//  GreenMatesTests
+//
+//  Created by base on 15/11/24.
+//
+
+import Testing
+@testable import GreenMates
+
+struct GreenMatesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
